b7f810ed48183f32bd7c03ec6231604642f47729
pgoergler <pgoergler@gmail.com>
2015-05-14 16:44:58 +0400

    Add \to_dateinterval and \dateinterval

--- src/datetime.php
+++ src/datetime.php
@@ -52,3 +52,46 @@ function now()
     }
     return new \DateTime();
 }
+
+/**
+ *
+ * @param Mixed $dateinterval
+ * @return \DateInterval
+ */
+function dateinterval($dateinterval, $timezone = null)
+{
+    $dependencyInjection = \OngooUtils\OngooUtils::getInstance()->getInjector();
+    if ($dependencyInjection && $dependencyInjection->OffsetExists('to_dateinterval'))
+    {
+        return $dependencyInjection['to_dateinterval']($dateinterval, $timezone);
+    }
+    return to_dateinterval($dateinterval, $timezone);
+}
+
+/**
+ *
+ * @param Mixed $dateinterval
+ * @return \DateInterval
+ */
+function to_dateinterval($dateinterval, $timezone = null)
+{
+    $tz = is_null($timezone) ? null : ($timezone instanceof \DateTimeZone ? $timezone : new \DateTimeZone($timezone));
+
+    if ($dateinterval instanceof \DateInterval)
+    {
+        return clone $dateinterval;
+    }
+
+    if (is_string($dateinterval) && preg_match("/^P/", $dateinterval))
+    {
+        return new \DateInterval($dateinterval);
+    }
+
+    if (is_string($dateinterval) && preg_match("/(?:([0-9]+) years? ?)?(?:([0-9]+) mons? ?)?(?:([0-9]+) days? ?)?(?:([0-9]{1,2}):([0-9]{1,2}):([0-9]+))?/", $dateinterval, $matchs))
+    {
+        return \DateInterval::createFromDateString(
+                        sprintf("%d years %d months %d days %d hours %d minutes %d seconds", array_key_exists(1, $matchs) ? (is_null($matchs[1]) ? 0 : (int) $matchs[1]) : 0, array_key_exists(2, $matchs) ? (is_null($matchs[2]) ? 0 : (int) $matchs[2]) : 0, array_key_exists(3, $matchs) ? (is_null($matchs[3]) ? 0 : (int) $matchs[3]) : 0, array_key_exists(4, $matchs) ? (is_null($matchs[4]) ? 0 : (int) $matchs[4]) : 0, array_key_exists(5, $matchs) ? (is_null($matchs[5]) ? 0 : (int) $matchs[5]) : 0, array_key_exists(6, $matchs) ? (is_null($matchs[6]) ? 0 : (int) $matchs[6]) : 0
+        ));
+    }
+    throw new \InvalidArgumentException('$time must be a string or a \DateInterval');
+}
